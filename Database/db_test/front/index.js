48b9e1cedc39b5d3c1d5adcd688ebb4d8097ac85
thelikeaninja <77681263+thelikeaninja@users.noreply.github.com>
2021-03-02 12:05:55 -0300

    test commit

--- Database/db_test/front/index.js
+++
@@ -1,6 +0,0 @@
- function get_data(dado, cont){
-    data = dado[cont];
-    document.getElementById('nome'+ cont).innerHTML = data['nome'];
-    document.getElementById('cpf'+ cont).innerHTML =  data['cpf'];
-
- };
